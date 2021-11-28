51385a95c4cd83b01b6492166c1eb0a3d31ddc30
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-11-28 02:23:18 -0500

    Worked on Binder Class

--- src/main/java/edu/temple/audiobb/AudioService.kt
+++ src/main/java/edu/temple/audiobb/AudioService.kt
@@ -6,19 +6,64 @@ import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
 import android.util.Log
+import java.io.File
 
 class AudioService : Service()
 {
-    var paused = false
+    var paused = false // paused = anytime not playing
     var shouldExit = false
-    lateinit var timerHandler : Handler
+    lateinit var audioHandler : Handler
 
     inner class AudioBinder : Binder()
     {
         fun playAudio(id: Int)
         {
-            play(id : int)
+            play(id)
+            paused = false
         }
+
+        fun playAudio(file: File)
+        {
+            play(file)
+            paused = false
+        }
+
+        fun playAudio(file : File, position : Int)
+        {
+            play(file, position)
+            paused = false
+        }
+
+        fun pauseAudio()
+        {
+            pause()
+            paused = true
+        }
+
+        fun stopAudio()
+        {
+            stop()
+            paused = true
+        }
+
+        fun setProgressHandler(progressHandler: Handler)
+        {
+
+        }
+
+        fun seekAudio(position : Int)
+        {
+            seekTo(position)
+        }
+
+        fun isPlaying() : Boolean
+        {
+            return paused
+        }
+
+
+
+
     }
 
 }
